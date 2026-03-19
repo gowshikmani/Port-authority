09d38decff4d62e2c37b42302f4d070ca5daab2b
gowshikmani <gowshiksanjeev@gmail.com>
2026-03-19 07:48:44 +0530

    ship.model has modified by adding contents

--- server/ships/ship.model.js
+++ server/ships/ship.model.js
@@ -1,1 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const shipSchema = new mongoose.Schema({
+  name: String,
+  imoNumber: String,
+  arrivalTime: Date,
+  status: String
+});
+
+module.exports = mongoose.model("Ship", shipSchema);
